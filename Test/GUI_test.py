CGPA Correct Formula Implementated

Test/GUI_test.py
@@ -81,16 +81,17 @@ def run_program():
             # Extract Names of Student from column 'B' (index 1) starting from row 4 (index 3)
             student_names = df_excel.iloc[3:, 1].reset_index(drop=True)
             # Extract Credits, SGPA and Results of all Students
-            credits = df_excel.iloc[2, 4:].reset_index(drop=True)
-            credits = credits.dropna()
-            total_subjects, total_credits = len(credits), sum(credits)
+            subject_codes = df_excel.iloc[0, 4:].reset_index(drop=True)
+            subject_codes = subject_codes.dropna().reset_index(drop=True).apply(lambda x:x.strip()) # Drop columns where all values are NaN
+            total_subjects = len(subject_codes)
+            credits = df_excel.iloc[2, 4:4+total_subjects].reset_index(drop=True)
+            total_credits = sum(credits)
             sgpa = df_excel.iloc[3:, 4 + total_subjects ].reset_index(drop=True)
             results = df_excel.iloc[3:, 5 + total_subjects].reset_index(drop=True)
 
             # Extract Subjects for all students
             subjects = df_excel.iloc[:,4:4 + total_subjects].reset_index(drop=True)
             subjects.columns = range(total_subjects)
-            subjects_id = subjects.iloc[0,:] # Extracting Subject IDs from first row of subjects
             subjects_names = subjects.iloc[1,:]
 
             # Extracting Grades and calculating grade points for all students
@@ -110,8 +111,10 @@ def run_program():
                                         else ('PASS' if x >= 4.0 else 'FAIL'))))
 
             # Calculating CGPA according to Semesters (Total SGPA till current Semester)/semester_number
-            total_sgpa = df_all_sem.loc[:len(roll_numbers)-1 ,'Sem1':'Sem'+str(sem_number)].sum(axis=1)
-            cgpa = round(total_sgpa/sem_number,3)
+            total_sem_sgpa = df_all_sem.loc[:len(roll_numbers)-1 ,'Sem1':'Sem'+str(sem_number)]
+            total_sem_credits = df_all_sem.loc[:0,'Credit_1':'Credit_'+str(sem_number)]
+            cgpa = total_sem_sgpa.mul(total_sem_credits.iloc[0].values, axis=1).sum(axis=1) / total_sem_credits.iloc[0].sum()
+            cgpa = cgpa.round(2)
 
         except Exception as e:
             messagebox.showerror("Error occurred while reading Student Details: ", e)
@@ -158,6 +161,11 @@ def run_program():
                 YEAR = int(batch.split('-')[1])
                 MONTH = "MAY"
 
+            provided_date = str(df_excel.iloc[0, 1]).strip()
+            if provided_date != "" and provided_date.lower() != 'nan':
+                _ , exam_month, exam_year = provided_date.split()
+                MONTH = exam_month.upper()
+                YEAR = int(exam_year)
         except Exception as e:
             messagebox.showerror("Error occurred while Configuring Month and Year: ", e)
             messagebox.showerror("Please check that Batch must be in correct format, Ex:'2000-2005'")
@@ -185,16 +193,23 @@ def run_program():
             messagebox.showerror("Error Occured while adding the rows in UTD file.")
             
         try:
+            # Extract Course Information for all students (Row index 0, Column index 1 assuming zero-based index)
+            course_name =  (pd.read_excel(all_sem_file, header=None)).iloc[0, 1]  # Row 2, Column B in Excel (0-based index)
+            # Extract stream Information for all students (Row index 1, Column index 1 assuming zero-based index)
+            stream =  (pd.read_excel(all_sem_file, header=None)).iloc[1, 1]  # Row 1, Column 1 in Excel (0-based index)
             # Step 4: Fill constant data for all students
             # -----------------------------
             # For each student (starting at row 3 i.e. index 2), fill in columns A-F with constant values.
-
+            if "five" in course_name.lower() or "dual" in course_name.lower():
+                course_full_name = "B.TECH.+M.TECH. (INTERNET OF THINGS) DUAL DEGREE 5 YRS."
+            else:
+                course_full_name = f"{course_name} {stream}"
             df_utd.iloc[2:required_rows, 0] = "DEVI AHILYA VISHWAVIDYALAYA INDORE"   # University Name (Column A)
-            df_utd.iloc[2:required_rows, 1] = "SCHOOL OF INSTRUMENTATION INDORE"     # College Name (Column B)
-            df_utd.iloc[2:required_rows, 2] = "INTEGRATED M.TECH (IOT)"              # Course Name in Short (Column C)
-            df_utd.iloc[2:required_rows, 3] = "INTEGRATED MASTER OF TECHNOLOGY"      # Full Course Name (Column D)
-            df_utd.iloc[2:required_rows, 4] = "INTEGRATED MASTER OF TECHNOLOGY"      # Full Course Name in Detail (Column E)
-            df_utd.iloc[2:required_rows, 5] = "INTERNET OF THINGS"                   # Stream (Column F)
+            df_utd.iloc[2:required_rows, 1] = "SCHOOL OF INSTRUMENTATION"     # College Name (Column B)
+            df_utd.iloc[2:required_rows, 2] = course_name                             # Course Name in Short (Column C)
+            df_utd.iloc[2:required_rows, 3] = course_full_name                        # Full Course Name (Column D)
+            df_utd.iloc[2:required_rows, 4] = course_full_name                        # Full Course Name in Detail (Column E)
+            df_utd.iloc[2:required_rows, 5] = stream                                   # Stream (Column F)
             df_utd.iloc[2:required_rows, 7] = batch                                  # Session for Batch  
             df_utd.iloc[2:required_rows, 18] = YEAR                                  # Year for Batch  
             df_utd.iloc[2:required_rows, 19] = MONTH                                 # MONTH for Batch  
@@ -246,7 +261,7 @@ def run_program():
             j=0
             for i in range(total_subjects):
                 df_utd.iloc[2:required_rows, 39 + j] = subjects_names[i]
-                df_utd.iloc[2:required_rows, 40 + j] = subjects_id[i]
+                df_utd.iloc[2:required_rows, 40 + j] = subject_codes[i]
                 df_utd.iloc[2:required_rows, 51 + j] = credits[i]
                 j+=15
 

UTD.py
@@ -82,8 +82,10 @@ try:
                                 else ('PASS' if x >= 4.0 else 'FAIL'))))
 
     # Calculating CGPA according to Semesters (Total SGPA till current Semester)/semester_number
-    total_sgpa = df_all_sem.loc[:len(roll_numbers)-1 ,'Sem1':'Sem'+str(sem_number)].sum(axis=1)
-    cgpa = round(total_sgpa/sem_number,3)
+    total_sem_sgpa = df_all_sem.loc[:len(roll_numbers)-1 ,'Sem1':'Sem'+str(sem_number)]
+    total_sem_credits = df_all_sem.loc[:0,'Credit_1':'Credit_'+str(sem_number)]
+    cgpa = total_sem_sgpa.mul(total_sem_credits.iloc[0].values, axis=1).sum(axis=1) / total_sem_credits.iloc[0].sum()
+    cgpa = cgpa.round(2)
 
 except Exception as e:
     print("Error occurred while reading Student Details: ", e)